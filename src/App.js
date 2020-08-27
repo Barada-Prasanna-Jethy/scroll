52890639a857d44035848b5a612594fd9f9c8a98
Barada-Prasanna-Jethy <baradajethy@gmail.com>
2020-08-27 14:22:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
-import Box from "./components/box";
 export default class App extends Component {
   state = {
-    redBoxes: []
+    arr: [<div className="container">hello world</div>]
   };
-  changeColor(i) {
-    console.log("click5ed");
-    var newBoxes = this.state.redBoxes;
-
-    if (this.state.redBoxes.length < 2) {
-      newBoxes.push(i);
-    } else {
-      newBoxes.push(i);
-      newBoxes.shift();
-    }
-
-    this.setState({ redBoxes: newBoxes });
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      this.handleScroll(this.state.arr);
+    });
   }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", () => {
+      this.handleScroll(this.state.arr);
+    });
+  }
+  handleScroll = arr => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    var newArray = arr;
+    newArray.push(newArray[0]);
+    console.log("hell");
+
+    this.setState({ array: newArray });
+  };
   render() {
-    const boxes = [];
-    const redBoxes = this.state.redBoxes;
-    for (let i = 0; i < 16; i++) {
-      if (redBoxes.includes(i)) boxes.push(<Box key={i} color="red" />);
-      else
-        boxes.push(
-          <Box
-            key={i}
-            index={i}
-            colorChange={index => {
-              this.changeColor(index);
-            }}
-            color="blue"
-          />
-        );
-    }
-    return <div className="container">{boxes}</div>;
+    return this.state.arr;
   }
 }
